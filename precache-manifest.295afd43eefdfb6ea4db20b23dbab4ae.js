3b81fe18d42339b6ba73d63fe2af6172dbcd7e84
bcm32 <bcm3223@gmail.com>
2020-07-30 13:23:54 -0400

    Updates

---
+++ precache-manifest.295afd43eefdfb6ea4db20b23dbab4ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed751e0981a5a11976cc549960c7301b",
+    "url": "/relics/index.html"
+  },
+  {
+    "revision": "c289b11d80530b993cd4",
+    "url": "/relics/static/css/main.cb2255b4.chunk.css"
+  },
+  {
+    "revision": "40f6d578d127b4ebee0f",
+    "url": "/relics/static/js/2.31c5ec36.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/relics/static/js/2.31c5ec36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c289b11d80530b993cd4",
+    "url": "/relics/static/js/main.a61c0a13.chunk.js"
+  },
+  {
+    "revision": "ca661c5e122b6206abe5",
+    "url": "/relics/static/js/runtime-main.ccb229eb.js"
+  }
+]);
